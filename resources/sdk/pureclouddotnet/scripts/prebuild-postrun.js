e2829dfafde9fa775675582a16f15eb9fa00dfac
jsfix.live analysis <info@coana.tech>
2022-10-11 16:52:34 +0200

    JSFIX patches

--- resources/sdk/pureclouddotnet/scripts/prebuild-postrun.js
+++ resources/sdk/pureclouddotnet/scripts/prebuild-postrun.js
@@ -5,56 +5,56 @@ const path = require('path');
 var Mustache = require('mustache');
 
 function generateNotificationTopicsFile(templatePath, dataPath, outPath, namespace) {
-	var notificationsRaw = fs.readFileSync(dataPath, 'UTF-8');
-	var notifications = JSON.parse(notificationsRaw);
-	notifications.namespace = namespace;
-	var notificationsTemplate = fs.readFileSync(templatePath, 'UTF-8');
+ var notificationsRaw = fs.readFileSync(dataPath, 'UTF-8');
+ var notifications = JSON.parse(notificationsRaw);
+ notifications.namespace = namespace;
+ var notificationsTemplate = fs.readFileSync(templatePath, 'UTF-8');
 
-	var notificationsClass = Mustache.render(notificationsTemplate, notifications);
-	fs.writeFileSync(outPath, notificationsClass, 'UTF-8');
-	console.log(`File written to ${outPath}`);
+ var notificationsClass = Mustache.render(notificationsTemplate, notifications);
+ fs.writeFileSync(outPath, notificationsClass, 'UTF-8');
+ console.log(`File written to ${outPath}`);
 }
 
 try {
-	var swaggerCodegenConfigFilePath = process.argv[2];
-	var version = require(path.resolve(process.argv[3]));
-	var packageName = process.argv[4];
-	var notificationsTemplatePath = process.argv[5];
-	var notificationsDataPath = process.argv[6];
-	var notificationsOutPath = process.argv[7];
-	var nugetPath = process.argv[8];
+ var swaggerCodegenConfigFilePath = process.argv[2];
+ var version = require(path.resolve(process.argv[3]));
+ var packageName = process.argv[4];
+ var notificationsTemplatePath = process.argv[5];
+ var notificationsDataPath = process.argv[6];
+ var notificationsOutPath = process.argv[7];
+ var nugetPath = process.argv[8];
 
-	console.log(`swaggerCodegenConfigFilePath=${swaggerCodegenConfigFilePath}`);
-	console.log(`version=${JSON.stringify(version)}`);
-	console.log(`packageName=${packageName}`);
-	console.log(`notificationsTemplatePath=${notificationsTemplatePath}`);
-	console.log(`notificationsDataPath=${notificationsDataPath}`);
-	console.log(`notificationsOutPath=${notificationsOutPath}`);
+ console.log(`swaggerCodegenConfigFilePath=${swaggerCodegenConfigFilePath}`);
+ console.log(`version=${JSON.stringify(version)}`);
+ console.log(`packageName=${packageName}`);
+ console.log(`notificationsTemplatePath=${notificationsTemplatePath}`);
+ console.log(`notificationsDataPath=${notificationsDataPath}`);
+ console.log(`notificationsOutPath=${notificationsOutPath}`);
 
-	var config = {
-		packageName: packageName || 'PureCloudPlatform.Client',
-		packageVersion: version.displayFull,
-		packageTitle: 'PureCloud Platform Client SDK',
-		packageProductName: 'PureCloudPlatformClient',
-		packageDescription: 'A .NET library to interface with the PureCloud Public API',
-		packageCompany: 'Genesys',
-		packageCopyright: 'Copyright © Genesys 2022',
-		httpUserAgent: 'PureCloud SDK',
-		targetFramework: 'net452'
-	};
+ var config = {
+  packageName: packageName || 'PureCloudPlatform.Client',
+  packageVersion: version.displayFull,
+  packageTitle: 'PureCloud Platform Client SDK',
+  packageProductName: 'PureCloudPlatformClient',
+  packageDescription: 'A .NET library to interface with the PureCloud Public API',
+  packageCompany: 'Genesys',
+  packageCopyright: 'Copyright © Genesys 2022',
+  httpUserAgent: 'PureCloud SDK',
+  targetFramework: 'net452'
+ };
 
-	fs.writeFileSync(swaggerCodegenConfigFilePath, JSON.stringify(config, null, 2));
-	console.log(`Config file written to ${swaggerCodegenConfigFilePath}`);
+ fs.writeFileSync(swaggerCodegenConfigFilePath, JSON.stringify(config, null, 2));
+ console.log(`Config file written to ${swaggerCodegenConfigFilePath}`);
 
-	generateNotificationTopicsFile(notificationsTemplatePath, notificationsDataPath, notificationsOutPath, packageName);
+ generateNotificationTopicsFile(notificationsTemplatePath, notificationsDataPath, notificationsOutPath, packageName);
 
-	console.log('downloading nuget...');
-	let data = cp.execFileSync('curl', ['--silent', '-L', 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'], {
-		encoding: 'binary',
-		maxBuffer: 1024 * 1024 * 10
-	});
-	fs.writeFileSync(nugetPath, data, 'binary');
+ console.log('downloading nuget...');
+ let data = cp.execFileSync('curl', ['--silent', '-L', 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'], {
+  encoding: 'binary',
+  maxBuffer: 1024 * 1024 * 10
+ });
+ fs.writeFileSync(nugetPath, data, 'binary');
 } catch (err) {
-	process.exitCode = 1;
-	console.log(err);
+ process.exitCode = 1;
+ console.log(err);
 }
